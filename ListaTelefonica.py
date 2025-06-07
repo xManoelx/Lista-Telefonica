7c8df2259b9ee3feaee8f021375665a3c0c962de
xManoelx <manoeldr03@gmail.com>
2025-06-07 12:19:01 -0300

    Estrutura Inicial

---
+++ ListaTelefonica.py
@@ -0,0 +1,31 @@
+while True:
+    print("\nMenu do Gerenciador da Lista de Telefonica")
+    print("1. Adicionar um novo contato")
+    print("2. Ver lista de contatos")
+    print("3. Favoritar contato")
+    print("4. Ver lista de favoritos")
+    print("5. Excluir contato")
+    print("6. Sair")
+
+    choose = input("\nDigite a sua escolha: ")
+
+    # Adicionar tarefa
+    # if choose == "1": 
+
+    # Ver tarefas
+    # elif choose == "2":    
+
+    # Atualizar tarefa
+    # elif choose == "3": 
+
+    # Completar tarefa 
+    # elif choose == "4":   
+
+    # Remover tarefa
+    # elif choose == "5": 
+
+    # Sair
+    if choose == "6":
+        break 
+
+print("Programa finalizado")
